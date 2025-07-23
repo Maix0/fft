dff178706b8e4de7f176c06ac688beec7d0bae15
Maix0 <maieul.dev@familleboyer.net>
2025-07-23 15:33:29 +0200

    update: if using AKM (aka prod) dont put port + fixed admin template

--- config.py
+++ config.py
@@ -29,6 +29,8 @@ db_path = os.environ.get("F42_DB", default="database.db")
 domain = os.environ.get("F42_DOMAIN")
 proxy_domain = os.environ.get("F42_PROXY_DOMAIN", default=domain)
 redirect_url = f"http://{domain}:{os.environ.get('F42_PORT')}/auth"
+if use_akm == "1":
+    redirect_url  = f"https://{domain}/auth"
 auth_link = f"https://api.intra.42.fr/oauth/authorize?client_id={key}&redirect_uri={redirect_url}&response_type=code&scope=public"
 redis_host = os.environ.get("F42_REDIS_HOST")
 redis_port = os.environ.get("F42_REDIS_PORT")
